28e4ba8d0174f0703a3e34096bf784d1dbfb182c
Vania Neves <vania.neves@gmail.com>
2020-02-27 06:21:31 -0300

    #7 Fixing to allow to inform more than one university

--- plugins/Abroad/config.inc.php
+++ plugins/Abroad/config.inc.php
@@ -2,7 +2,7 @@
 /**
  * Plugin configuration interface
  *
- * @package REST_API plugin
+ * @package Abroad plugin
  */
 
 // Check the script is called by the right program & plugin is activated.
@@ -20,86 +20,179 @@ if ( $_REQUEST['modname'] !== 'School_Setup/Configuration.php'
 if ( isset( $_REQUEST['save'] )
 	&& $_REQUEST['save'] === 'true' )
 {
-	if ( $_REQUEST['values']['PROGRAM_USER_CONFIG']
-		&& $_POST['values']
-		&& AllowEdit() )
-	{
-		// Update the PROGRAM_USER_CONFIG table.
-		$sql = '';
+    
+    if ( ! empty( $_REQUEST['values'] )
+        && ! empty( $_POST['values'] )
+        && AllowEdit() )
+    {
+        foreach ( (array) $_REQUEST['values'] as $request_id => $columns )
+        {
+            $sql = "UPDATE UNIVERSITIES_ABROAD SET ";
+            
+            foreach ( (array) $columns as $column => $value )
+            {
+                $sql .= DBEscapeIdentifier( $column ) . "='" . $value . "',";
+            }
+            
+            $sql = mb_substr( $sql, 0, -1 ) .
+            " WHERE UNIVERSITY_ID='" . $request_id . "'";
+            
+           
+            DBQuery( $sql );
+        }
+    }
+    
 
-		if ( isset( $_REQUEST['values']['PROGRAM_USER_CONFIG'] )
-			&& is_array( $_REQUEST['values']['PROGRAM_USER_CONFIG'] ) )
-		{
-			foreach ( (array) $_REQUEST['values']['PROGRAM_USER_CONFIG'] as $column => $value )
-			{
-				ProgramUserConfig( 'Abroad', User( 'STAFF_ID' ), array( $column => $value ) );
-			}
-		}
-
-		if ( $sql != '' )
-		{
-			DBQuery( $sql );
-
-			$note[] = button( 'check' ) . '&nbsp;' . _( 'The plugin configuration has been modified.' );
-		}
-	}
 
 	// Unset save & values & redirect URL.
 	RedirectURL( 'save', 'values' );
 }
 
 
-if ( empty( $_REQUEST['save'] ) )
+// New.
+if ( isset( $_REQUEST['new'] )
+    && $_REQUEST['new'] === 'true' )
 {
-
-
-	echo '<form action="Modules.php?modname=' . $_REQUEST['modname'] . '&tab=plugins&modfunc=config&plugin=Abroad&save=true" method="POST">';
-
-	DrawHeader( '', SubmitButton( _( 'Save' ) ) );
-
-	echo ErrorMessage( $note, 'note' );
-
-	echo ErrorMessage( $error, 'error' );
-	
-	$api_config = ProgramUserConfig( 'Abroad' );
-	
-	
-	$api_uni_name = ! empty( $api_config['UNIVERSITY_NAME'] ) ? $api_config['UNIVERSITY_NAME'] : '';
-	$api_uni_url = ! empty( $api_config['UNIVERSITY_URL'] ) ? $api_config['UNIVERSITY_URL'] : '';
-
-
-	echo '<br />';
-
-	PopTable(
-		'header',
-		dgettext( 'Abroad', 'Abroad' )
-	);
-	
-
-	echo '<table class="width-100p">';
-
-	// University Name
-	echo '<tr><td>' . TextInput(
-	    $api_uni_name,
-	    'values[PROGRAM_USER_CONFIG][UNIVERSITY_NAME]',
-	    _( 'University Name' ),
-	    'size=100 '
-	    ) . '</td></tr>';
-
     
-	    // University API URL
-	    echo '<tr><td>' . TextInput(
-	        $api_uni_url,
-	        'values[PROGRAM_USER_CONFIG][UNIVERSITY_URL]',
-	        _( 'API URL' ),
-	        'size=100 '
-	        ) . '</td></tr>';
-	    
+    if ( ! empty( $_POST ) && AllowEdit() )
+    {
 
-	echo '</table>';
+        DBQuery( "INSERT INTO UNIVERSITIES_ABROAD (UNIVERSITY_NAME, UNIVERSITY_URL, UNIVERSITY_TOKEN)
+			VALUES ('" . $_POST['UNIVERSITY_NAME'] . "', '" . $_POST['UNIVERSITY_URL'] . "', '" . $_POST['UNIVERSITY_TOKEN'] . "')" );
+        
+    }
+        // Unset modfunc & ID & redirect URL.
+        RedirectURL( array('add', 'new', 'values') );
+}
 
-	PopTable( 'footer' );
+// Remove.
+if ( isset( $_REQUEST['remove'] )
+    && $_REQUEST['remove'] === 'true' )
+{
+    if ( DeletePrompt( _( 'Request' ) ) )
+    {
+        DBQuery( "DELETE FROM UNIVERSITIES_ABROAD
+			WHERE UNIVERSITY_ID='" . $_REQUEST['id'] . "'" );
+        
+        // Unset modfunc & ID & redirect URL.
+        RedirectURL( 'remove', 'values' );
+    }
+}
 
-	echo '<br /><div class="center">' . SubmitButton( _( 'Save' ) ) . '</div></form>';
+
+// Add.
+if ( isset( $_REQUEST['add'] )
+    && $_REQUEST['add'] === 'true' )
+{
+    
+    PopTable(
+        'header',
+        dgettext( 'University Abroad', 'University Abroad' )
+        );
+    
+    echo '<form action="Modules.php?modname=' . $_REQUEST['modname'] . '&tab=plugins&modfunc=config&plugin=Abroad&new=true" method="POST">';
+    echo '<table class="width-100p">';
+    
+    // University Name
+    echo '<tr><td>' . TextInput(
+        '',
+        'UNIVERSITY_NAME',
+        _( 'University Name' ),
+        'size=100 '
+        ) . '</td></tr>';
+        
+        
+        // University API URL
+        echo '<tr><td>' . TextInput(
+            '',
+            'UNIVERSITY_URL',
+            _( 'API URL' ),
+            'size=100 '
+            ) . '</td></tr>';
+            
+            // University TOKEN
+            echo '<tr><td>' . TextInput(
+                '',
+                'UNIVERSITY_TOKEN',
+                _( 'Token' ),
+                'size=100 '
+                ) . '</td></tr>';
+            echo '</table>';
+            
+            echo '<br /><div class="center">' . SubmitButton( _( 'Save' ) ) . '</div></form>';
+            
+            PopTable( 'footer' );
+            
+            
+            
+}
+
+
+if ( empty( $_REQUEST['save']) && 
+    empty( $_REQUEST['remove'])&&
+    empty( $_REQUEST['add'])&&
+    empty( $_REQUEST['new']))
+{
+    
+    $functions = array(
+        'UNIVERSITY_NAME' => '_makeUniversity',
+        'UNIVERSITY_URL' => '_makeUniversity',
+        'UNIVERSITY_TOKEN' => '_makeUniversity',
+    );
+    
+    $requests_RET = DBGet( "SELECT * FROM UNIVERSITIES_ABROAD ", $functions );
+    
+    $columns = array(
+        'UNIVERSITY_NAME' => _( 'University Name' ),
+        'UNIVERSITY_URL' => _( 'University URL API' ),
+        'UNIVERSITY_TOKEN' => _( 'University Token' ),
+    );
+    
+    $link['remove'] = array(
+        'link' => 'Modules.php?modname=' . $_REQUEST['modname'] . '&tab=plugins&modfunc=config&plugin=Abroad&remove=true',
+        'variables' => array( 'id' => 'UNIVERSITY_ID' ),
+    );
+    
+    $link['add'] = array(
+        'link' => 'Modules.php?modname=' . $_REQUEST['modname'] . '&tab=plugins&modfunc=config&plugin=Abroad&add=true',
+    );
+    $link['add']['title'] = _( 'Add University Abroad' );
+    echo '<form action="Modules.php?modname=' . $_REQUEST['modname'] . '&tab=plugins&modfunc=config&plugin=Abroad&save=true" method="POST">';
+    
+    
+    ListOutput(
+        $requests_RET,
+        $columns,
+        'Request',
+        'Requests',
+        $link
+        );
+    
+
+
+    echo '</div>';
+    
+    echo '<br /><div class="center">' . SubmitButton() . '</div>';
+    echo '</form>';
+    
+
+
+
+}
+
+
+function _makeUniversity( $value, $column )
+{
+    global $THIS_RET;
+    
+   
+    return TextInput(
+        $value,
+        'values[' . $THIS_RET['UNIVERSITY_ID'] . ']['.$column.']',
+        '',
+        'size=50 ',
+        false
+        );
+
 }
 
